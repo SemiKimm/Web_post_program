7231b848f4cc2e5819edda136b7d8fc5f6ac3789
김세미 <semikimm.m@gmail.com>
2022-04-30 21:11:01 +0900

    [webapp_final]step1 - Post 테스트&구현 완료

--- web_post_program_kimsemi/src/test/java/com/nhnacademy/post/project/domain/PostTest.java
+++ web_post_program_kimsemi/src/test/java/com/nhnacademy/post/project/domain/PostTest.java
@@ -1,61 +1,83 @@
 package com.nhnacademy.post.project.domain;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import java.time.LocalDateTime;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class PostTest {
+    Post post;
 
     @BeforeEach
     void setUp() {
+        post = new PostingServicePost();
     }
 
     @Test
-    void getId() {
+    void setIdAndGetId() {
+        long testId = 1L;
+        post.setId(testId);
+
+        long result = post.getId();
+        assertThat(result)
+            .isNotNull()
+            .isEqualTo(1L);
     }
 
     @Test
-    void setId() {
+    void setTitleAndGetTitle() {
+        String testTitle = "testPost";
+        post.setTitle(testTitle);
+
+        String result = post.getTitle();
+        assertThat(result)
+            .isNotNull()
+            .isEqualTo("testPost");
+    }
+
+
+    @Test
+    void setContentAndGetContent() {
+        String testContent = "hello, world";
+        post.setContent(testContent);
+
+        String result = post.getContent();
+        assertThat(result)
+            .isNotNull()
+            .isEqualTo("hello, world");
+    }
+
+
+    @Test
+    void setWriterUserIdAndGetWriterUserId() {
+        String testWriterUserId = "semi";
+        post.setWriterUserId(testWriterUserId);
+
+        String result = post.getWriterUserId();
+        assertThat(result)
+            .isNotNull()
+            .isEqualTo("semi");
     }
 
     @Test
-    void getTitle() {
+    void setWriteTimeAndGetWriteTime() {
+        LocalDateTime testLocalDateTime = LocalDateTime.now();
+        post.setWriteTime(testLocalDateTime);
+
+        LocalDateTime result = post.getWriteTime();
+        assertThat(result)
+            .isNotNull()
+            .isEqualTo(testLocalDateTime);
     }
 
     @Test
-    void setTitle() {
-    }
+    void increaseViewCountAndGetViewCount() {
+        post.increaseViewCount();
 
-    @Test
-    void getContent() {
-    }
-
-    @Test
-    void setContent() {
-    }
-
-    @Test
-    void getWriterUserId() {
-    }
-
-    @Test
-    void setWriterUserId() {
-    }
-
-    @Test
-    void getWriteTime() {
-    }
-
-    @Test
-    void setWriteTime() {
-    }
-
-    @Test
-    void getViewCount() {
-    }
-
-    @Test
-    void increaseViewCount() {
+        int result = post.getViewCount();
+        assertThat(result)
+            .isNotZero()
+            .isEqualTo(1);
     }
 }
