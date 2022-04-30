daaca8aa3b5222cfe6a75441fd740b79fd2d6784
김세미 <semikimm.m@gmail.com>
2022-04-30 20:51:22 +0900

    [webapp_final]step1 - Post 테스트&구현 시작

---
+++ web_post_program_kimsemi/src/test/java/com/nhnacademy/post/project/domain/PostTest.java
@@ -0,0 +1,61 @@
+package com.nhnacademy.post.project.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class PostTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void setId() {
+    }
+
+    @Test
+    void getTitle() {
+    }
+
+    @Test
+    void setTitle() {
+    }
+
+    @Test
+    void getContent() {
+    }
+
+    @Test
+    void setContent() {
+    }
+
+    @Test
+    void getWriterUserId() {
+    }
+
+    @Test
+    void setWriterUserId() {
+    }
+
+    @Test
+    void getWriteTime() {
+    }
+
+    @Test
+    void setWriteTime() {
+    }
+
+    @Test
+    void getViewCount() {
+    }
+
+    @Test
+    void increaseViewCount() {
+    }
+}
