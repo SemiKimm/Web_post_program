389f6b523020738fc198b7da5fcdddcfaa314c7b
김세미 <semikimm.m@gmail.com>
2022-04-30 23:02:55 +0900

    [webapp_final] step-1 : servletContext 초기화 시점에 관리자, userRepo, postRepo 등록

---
+++ web_post_program_kimsemi/src/main/java/com/nhnacademy/post/project/listener/WebAppListener.java
@@ -0,0 +1,37 @@
+package com.nhnacademy.post.project.listener;
+
+import com.nhnacademy.post.project.domain.PostRepository;
+import com.nhnacademy.post.project.domain.PostingServicePostRepository;
+import com.nhnacademy.post.project.domain.PostingServiceUser;
+import com.nhnacademy.post.project.domain.PostingServiceUserRepository;
+import com.nhnacademy.post.project.domain.User;
+import com.nhnacademy.post.project.domain.UserRepository;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class WebAppListener implements ServletContextListener {
+    PostRepository postRepository;
+    UserRepository userRepository;
+    User adminUser;
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        ServletContext context = sce.getServletContext();
+        postRepository = new PostingServicePostRepository();
+        userRepository = new PostingServiceUserRepository();
+        adminUser = new PostingServiceUser();
+        adminUser.setId("admin");
+        adminUser.setPassword("12345");
+        adminUser.setName("관리자");
+
+        context.setAttribute("postRepository", postRepository);
+        context.setAttribute("userRepository", userRepository);
+        context.setAttribute("admin", adminUser);
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+
+    }
+}
